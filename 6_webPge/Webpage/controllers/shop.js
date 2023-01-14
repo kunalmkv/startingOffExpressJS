dc9225df478d386893a661426554d25e669ea772
kunalmkv <kunalm4.kvtagore@gmail.com>
2023-01-14 14:41:09 +0530

    task 11

--- 6_webPge/Webpage/controllers/shop.js
+++ 6_webPge/Webpage/controllers/shop.js
@@ -27,17 +27,29 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 exports.postCart = (req, res, next) => {
 
   const prodId = req.body.productId;
-  console.log('456', prodId);
   Product.findById(prodId, product => {
-    console.log('123', product);
     Cart.addProduct(prodId, product.price);
   })
   res.redirect('/cart');
@@ -46,6 +58,13 @@ exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
     pageTitle: 'Your Orders'
+  });
+};
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
   });
 };
 
