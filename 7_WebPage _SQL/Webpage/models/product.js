190e09927a1104ab86da8368fa5678743fedb3d6
kunalmkv <kunalm4.kvtagore@gmail.com>
2023-01-17 17:06:07 +0530

    Sequelize 1

--- 7_WebPage _SQL/Webpage/models/product.js
+++ 7_WebPage _SQL/Webpage/models/product.js
@@ -1,4 +1,4 @@
-const db = require('../util/database');
+/*const db = require('../util/database');
 const fs = require('fs');
 const path = require('path');
 const Cart = require('./cart');
@@ -13,8 +13,6 @@ module.exports = class Product {
     this.description = description;
     this.price = price;
   }
-
-
   save() {
     return db.execute('INSERT INTO products(title, price, imageUrl, description) VALUES (?, ?, ?, ?)', [this.title, this.price, this.imageUrl, this.description]);
   }
@@ -28,4 +26,31 @@ module.exports = class Product {
   static findById(id) {
     return db.execute('SELECT * FROM products WHERE products.id=?', [id]);
   }
-};
+};*/
+
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+module.exports = Product;
--- 7_WebPage _SQL/Webpage/util/database.js
+++ 7_WebPage _SQL/Webpage/util/database.js
@@ -1,8 +1,11 @@
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node_complete',
     password: 'India@123'
 })
-module.exports = pool.promise();
+module.exports = pool.promise();*/
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node_complete', 'root', 'India@123', { dialect: 'mysql', host: 'localhost' });
+module.exports = sequelize;
